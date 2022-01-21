0a9ed6c053b7929ca20b4fef62cd8b39be194771
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-01-20 22:10:10 -0300

    :sparkles: FEATURE: Add example
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

---
+++ example/lib/data.dart
@@ -0,0 +1,114 @@
+import 'dart:math';
+
+class User {
+  final int index;
+  final String name;
+  final String surname;
+  final int points;
+  final DateTime createdAt;
+  final List<String> interests;
+
+  User({
+    required this.index,
+    required this.name,
+    required this.surname,
+    required this.points,
+    required this.createdAt,
+    required this.interests,
+  });
+}
+
+const _names = [
+  'Oliver',
+  'Jake',
+  'Noah',
+  'James',
+  'Jack',
+  'Connor',
+  'Liam',
+  'John',
+  'Harry',
+  'Callum',
+  'Mason',
+  'Robert',
+  'Michael',
+  'Charlie',
+  'Kyle',
+  'William',
+  'Thomas',
+  'Joe',
+  'Ethan',
+  'David',
+  'George',
+  'Reece',
+  'Michael',
+  'Richard',
+  'Oscar',
+  'Alexander',
+  'Joseph',
+  'James',
+  'Charlie',
+  'James',
+  'Charles',
+  'William',
+  'Damian',
+  'Daniel',
+  'Thomas',
+];
+
+const _surnames = [
+  'Smith',
+  'Murphy',
+  'Smith',
+  'Jones',
+  'O\'Kelly',
+  'Johnson',
+  'Williams',
+  'O\'Sullivan',
+  'Williams',
+  'Brown',
+  'Walsh',
+  'Brown',
+  'Taylor',
+  'Smith',
+  'Jones',
+  'Davies',
+  'O\'Brien',
+  'Miller',
+  'Wilson',
+  'Byrne',
+  'Davis',
+  'Evans',
+  'O\'Ryan',
+  'Garcia',
+  'Thomas',
+  'O\'Connor',
+  'Rodriguez',
+  'Roberts',
+  'O\'Neill',
+  'Wilson',
+];
+
+final _random = Random();
+String _createName() => _names[_random.nextInt(_names.length)];
+String _createSurname() => _surnames[_random.nextInt(_surnames.length)];
+
+Future<List<User>> createUsers() {
+  return Future.delayed(
+    const Duration(seconds: 3),
+    () => List.generate(
+      500,
+      (index) => User(
+        index: index,
+        name: _createName(),
+        surname: _createSurname(),
+        createdAt: DateTime.now().subtract(Duration(minutes: index * 30)),
+        points: index * 5,
+        interests: List.generate(
+          _random.nextInt(30),
+          (_) => '${_createName()} ${_createSurname()}',
+        ),
+      ),
+    ),
+  );
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:example/table.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Scalable Data Table',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Expanded(child: UsersTable()),
+        ],
+      ),
+    );
+  }
+}
